8722a0e3a240770d3d3db06504fd8a81e3c52a11
woltenX <wolten@gmail.com>
2018-05-03 18:15:41 -0600

    Primeras modificaciones

--- resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
@@ -4,9 +4,9 @@
     <!-- Logo -->
     <a href="{{ url('/home') }}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>A</b>LT</span>
+        <span class="logo-mini"><i class="fa fa-shield" aria-hidden="true"></i></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Admin</b>LTE Laravel </span>
+        <span class="logo-lg"> <i class="fa fa-shield" aria-hidden="true"></i> <b>Sentinel</b></span>
     </a>
 
     <!-- Header Navbar -->
