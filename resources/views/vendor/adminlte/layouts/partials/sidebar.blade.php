fb6d0d5e70fcf82cb31547a0b9de850cf5994981
woltenx <wolten@gmail.com>
2018-05-22 16:31:14 -0600

    Creado el filesystems para almacenaje de los  DEX

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -11,13 +11,14 @@
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>Dashboard</span></a></li>
+            <li><a href="#"><i class='fa fa-link'></i> <span>Etiquetas</span></a></li>
+            <li><a href="#"><i class='fa fa-link'></i> <span>Monitores</span></a></li>
             <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <a href="#"><i class='fa fa-cogs'></i> <span>Configuración</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
+                    <li><a href="#">Mi cuenta</a></li>
+                    <li><a href="#">Notificaciones</a></li>
                 </ul>
             </li>
         </ul><!-- /.sidebar-menu -->
